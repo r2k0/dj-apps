d33ab135323788efcf209219e403f7b77096d757
r2k0 <iheartcpp@gmail.com>
2014-09-15 12:03:05 -0700

    adding template and view function

--- twdp/rango/views.py
+++ twdp/rango/views.py
@@ -1,7 +1,18 @@
 from django.http import HttpResponse
+from django.template import RequestContext
+from django.shortcuts import render_to_response
 
 def index(request):
-	return HttpResponse("Rango says hello!<a href='/rango/about'>About</a>")
+	# context contains info such as the client's machine details
+	# request the context of the request
+	context = RequestContext(request)
+
+	# dictionary to pass to the template engine as its context
+	context_dict = {'boldmessage': "I am bold font from the context"}
+	
+	# return rendered response to send to the client
+	# render_to_response( template, context_dict, context )
+	return render_to_response('rango/index.html',context_dict, context)
 
 def about(request):
 	return HttpResponse("About. <a href='/rango/'>Index</a>")
--- twdp/twdp/settings.py
+++ twdp/twdp/settings.py
@@ -110,11 +110,12 @@ ROOT_URLCONF = 'twdp.urls'
 
 # Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = 'twdp.wsgi.application'
-
+TEMPLATE_PATH = os.path.join(PROJECT_PATH, 'templates')
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+	TEMPLATE_PATH,
 )
 
 INSTALLED_APPS = (

b5b1468483dbd3e5ebb312e7c57e751a198ff604
r2k0 <iheartcpp@gmail.com>
2014-11-04 22:20:29 -0800

    updates

--- t_project/t_project/settings.py
+++ t_project/t_project/settings.py
@@ -15,6 +15,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SETTINGS_DIR = os.path.dirname(__file__)
 PROJECT_PATH = os.path.join(SETTINGS_DIR, os.pardir)
 PROJECT_ROOT = os.path.abspath(PROJECT_PATH)
+TIME_ZONE = 'America/Los_Angeles'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
